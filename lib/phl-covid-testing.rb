4c009df582518c2f2b7c21b7dbd6d5732a7bb9d1
Brian Firestone <bfirestone2339@gmail.com>
2021-06-02 08:28:29 -0700

    Update filenames

---
+++ lib/phl-covid-testing.rb
@@ -0,0 +1,19 @@
+require 'pry'
+require 'colorize'
+require 'open-uri'
+require 'json'
+require 'net/http'
+
+require_relative "./concerns/Displayable"
+require_relative "./concerns/Findable"
+require_relative "./concerns/Inputable"
+
+require_relative "./phl_covid_testing/version"
+require_relative "./phl_covid_testing/cli"
+require_relative "./phl_covid_testing/api"
+require_relative "./phl_covid_testing/testing_location"
+
+
+module PHLCovidTesting
+  class Error < StandardError; end
+end
